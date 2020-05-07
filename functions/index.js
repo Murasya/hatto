3dc7d25dd99d5c021969835bfa2e04e63131e579
Murasya <vrydy.hppnss@icloud.com>
2020-05-07 20:30:34 +0900

    WKWebView用のパッケージを追加

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+const admin = require('firebase-admin');
 const gmailEmail = functions.config().gmail.email
 const gmailPassword = functions.config().gmail.password
 const mailTransport = nodemailer.createTransport({
@@ -23,4 +24,28 @@ exports.sendMail = functions.https.onCall((data, context) => {
         }
         return console.log('success')
     })
+})
+
+admin.initializeApp();
+exports.qandaList = functions.https.onRequest((request, response) => {
+    const json2csv = require("json2csv").parse;
+    const db = admin.firestore();
+    const ordersRef = db.collection('questionAndAnswer');
+    return ordersRef.get().then((querySnapshot) => {
+        const orders = [];
+        querySnapshot.forEach(doc => {
+            const order = doc.data();
+            orders.push(order);
+        });
+        response.setHeader(
+            "Content-disposition",
+            "attachment; filename=qanda.json"
+        );
+        response.set("Content-Type", "application/json");
+        response.status(200).send(orders);
+        return ""
+    }).catch((err) => {
+        response.status(200).send("エラー発生：" + err);
+        return Promise.resolve();
+    });
 })
