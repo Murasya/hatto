3735c5cdee8bf25818ff87d2c478d6376e91d84c
Murasya <vrydy.hppnss@icloud.com>
2020-05-07 20:40:09 +0900

    my-toolbar を追加

--- www/js/app.js
+++ www/js/app.js
@@ -197,3 +197,58 @@ Vue.component("skip-modal", {
     }
   }
 });
+
+Vue.component("my-toolbar", {
+  template: `
+    <div class="toolbar_wrapper">
+      <div class="toolbar">
+        <div @click="goAnalyze">
+          <img src="images/タブバー_新アイコン/analysis_アイコン.png"></img>
+          <span>分析</span>
+        </div>
+        <div @click="goHome">
+          <img src="images/タブバー_新アイコン/home_アイコン.png"></img>
+          <span>ホーム</span>
+        </div>
+        <div @click="goArchive">
+          <img src="images/タブバー_新アイコン/archives_アイコン.png"></img>
+          <span>アーカイブ</span>
+        </div>
+      </div>
+    </div>
+  `,
+  methods: {
+    goAnalyze: function() {
+      document.getElementById("myNavigator").resetToPage("analyze.html");
+    },
+    goHome: function() {
+      document.getElementById("myNavigator").resetToPage("index.html");
+    },
+    goArchive: function() {
+      document.getElementById("myNavigator").resetToPage("myRecode.html");
+    }
+  }
+});
+
+
+// admob
+var admobid = {};
+if( /(android)/i.test(navigator.userAgent) ) {
+    admobid = { // for Android
+        banner: 'ca-app-pub-3940256099942544/6300978111',
+        interstitial: 'ca-app-pub-3940256099942544/1033173712',
+        rewardvideo: 'ca-app-pub-3940256099942544/5224354917',
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-3940256099942544/2934735716',
+        interstitial: 'ca-app-pub-3940256099942544/4411468910',
+        rewardvideo: 'ca-app-pub-3940256099942544/1712485313',
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-6869992474017983/8878394753',
+        interstitial: 'ca-app-pub-6869992474017983/1355127956',
+        rewardvideo: '',
+    };
+}
